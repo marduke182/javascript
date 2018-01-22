a1602eca3de114f420646f2f1af1299e1f0406bd
Jesus Quintana <jrqb182@gmail.com>
2018-01-22 08:54:52 +0100

    feat(neutrino-jest-enzyme): create

---
+++ packages/neutrino-jest-enzyme/src/index.js
@@ -0,0 +1,13 @@
+module.exports = (neutrino, opts = {}) => {
+  neutrino.use(
+    '@neutrinojs/jest',
+    Object.assign(
+      {
+        testMatch: ['**/__tests__/**/*.test.js?(x)', '**/?(*.)(spec|test).js?(x)'],
+        testRegex: '',
+        setupTestFrameworkScriptFile: require.resolve('./framework'),
+      },
+      opts,
+    ),
+  );
+};
